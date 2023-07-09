73ac4c20a0de8514f22d5aa10e976e73dd2a3fdd
Eito Takase <eito01178@gmail.com>
2023-07-09 15:09:38 +0900

    app to core

--- src/newtab/Newtab.tsx
+++ src/newtab/Newtab.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
 
 import { Clock } from './components/Clock';
 import { ContributesGraph } from './components/ContributesGraph';
